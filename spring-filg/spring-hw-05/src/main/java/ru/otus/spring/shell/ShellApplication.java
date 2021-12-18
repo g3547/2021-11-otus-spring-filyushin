f824504313fc3f8980f54cd94b1087fddef89132
Georgiy Filyushin <gg3547@yandex.ru>
2021-12-18 14:01:42 +0300

    hw-5 init commit

---
+++ spring-filg/spring-hw-05/src/main/java/ru/otus/spring/shell/ShellApplication.java
@@ -0,0 +1,11 @@
+package ru.otus.spring.shell;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.shell.standard.ShellComponent;
+
+@ShellComponent
+@RequiredArgsConstructor
+public class ShellApplication {
+
+
+}
