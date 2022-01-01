75967766893a2927f4c366fceed4cc6bfe51c739
Georgiy Filyushin <gg3547@yandex.ru>
2022-01-01 09:37:38 +0300

    hw-6 delete service

--- spring-filg/spring-hw-06/src/main/java/ru/otus/spring/service/AuthorService.java
+++
@@ -1,9 +0,0 @@
-package ru.otus.spring.service;
-
-import ru.otus.spring.domain.Author;
-
-import java.util.List;
-
-public interface AuthorService {
-    List<Author> getAuthors();
-}
--- spring-filg/spring-hw-06/src/main/java/ru/otus/spring/service/GenreService.java
+++
@@ -1,9 +0,0 @@
-package ru.otus.spring.service;
-
-import ru.otus.spring.domain.Genre;
-
-import java.util.List;
-
-public interface GenreService {
-    List<Genre> getGenres();
-}
