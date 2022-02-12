dd875ad0e136cd6e721a0c4e547514196560a162
Georgiy Filyushin <gg3547@yandex.ru>
2022-02-12 13:41:35 +0300

    hw-10-mvc-spa init

---
+++ spring-filg/spring-hw-10-mvc-spa/src/main/java/ru/otus/spring/domain/Book.java
@@ -0,0 +1,49 @@
+package ru.otus.spring.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+import lombok.ToString;
+import org.hibernate.annotations.BatchSize;
+import org.hibernate.annotations.Fetch;
+import org.hibernate.annotations.FetchMode;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@Entity
+@Data
+@Table(name = "BOOK")
+@AllArgsConstructor
+public class Book {
+    @Id
+    @Column(name = "BOOK_ID")
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "SEQ_BOOK")
+    @SequenceGenerator(name = "SEQ_BOOK", allocationSize = 1)
+    private long id;
+
+    @Column(name = "TITLE")
+    private String title;
+
+    @OneToOne(targetEntity = Author.class)
+    @JoinColumn(name = "AUTHOR_ID")
+    @Fetch(FetchMode.SELECT)
+    @BatchSize(size = 5)
+    private Author author;
+
+    @OneToOne(targetEntity = Genre.class)
+    @JoinColumn(name = "GENRE_ID")
+    @Fetch(FetchMode.SELECT)
+    @BatchSize(size = 5)
+    private Genre genre;
+
+    @OneToMany(targetEntity = Comment.class, mappedBy = "book", cascade = CascadeType.ALL)
+    @ToString.Exclude
+    @Fetch(FetchMode.SELECT)
+    @BatchSize(size = 5)
+    private List<Comment> comments;
+
+    public Book(long id, String title, Author author, Genre genre) {
+        this(id, title, author, genre, null);
+    }
+}
---
+++ spring-filg/spring-hw-10-mvc-spa/src/main/java/ru/otus/spring/domain/Genre.java
@@ -0,0 +1,24 @@
+package ru.otus.spring.domain;
+
+import com.sun.istack.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "GENRE")
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class Genre {
+    @Column(name = "GENRE_ID")
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "SEQ_COMMENT")
+    @SequenceGenerator(name = "SEQ_GENRE", allocationSize = 1)
+    private long id;
+
+
+    @NotNull
+    @Column(name = "NAME")
+    private String name;
+}
